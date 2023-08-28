1fc7091259c62c1d296502da14027e97621fd3ed
Wutipong Wongsakuldej <mr_tawan@hotmail.com>
2023-08-28 15:47:38 +0700

    restructuring around search params

--- src/routes/browse/+page.svelte
+++ src/routes/browse/+page.svelte
@@ -27,6 +27,7 @@
 	import Pagination from './Pagination.svelte';
 	import Toast from '$lib/Toast.svelte';
 	import Toolbar from './Toolbar.svelte';
+	import { goto } from '$app/navigation';
 
 	interface Request {
 		favorite_only: boolean;
@@ -64,15 +65,8 @@
 	let toast: Toast;
 	let aboutDialog: AboutDialog;
 
-	let request: Request = {
-		favorite_only: false,
-		item_per_page: 30,
-		order: 'descending',
-		page: 0,
-		search: '',
-		sort: 'createTime',
-		tag: ''
-	};
+	let request = defaultRequest();
+
 	let response: Response = {
 		items: [],
 		pages: [],
@@ -82,10 +76,23 @@
 
 	let promise: Promise<void>;
 	onMount(() => {
-		promise = loadData();
+		parseParams();
+		invalidate(request);
 	});
 
-	async function loadData() {
+	function defaultRequest(): Request {
+		return {
+			favorite_only: false,
+			item_per_page: 30,
+			order: 'descending',
+			page: 0,
+			search: '',
+			sort: 'createTime',
+			tag: ''
+		};
+	}
+
+	function parseParams() {
 		const params = $page.url.searchParams;
 		if (params.has('favorite_only')) {
 			request.favorite_only = params.get('favorite_only') == 'true';
@@ -110,35 +117,32 @@
 				request.page = parseInt(v);
 			}
 		}
+	}
 
+	async function loadData() {
 		let u = new URL('/browse', getBackendBaseURL());
 		const r = await fetch(u, { method: 'POST', body: JSON.stringify(request) });
 		response = await r.json();
 	}
 
-	function changeSort(sortBy: string) {
-		let url = $page.url;
-		let searchParams = new URLSearchParams(url.search);
-		searchParams.set('sort', sortBy);
-
-		if (sortBy === 'name') {
-			searchParams.set('order', 'ascending');
-		} else if (sortBy === 'createTime') {
-			searchParams.set('order', 'descending');
+	function changeSort(sortBy: 'name' | 'createTime') {
+		request.sort = sortBy;
+		switch (sortBy) {
+			case 'name':
+				request.order = 'ascending';
+			case 'createTime':
+				request.order = 'descending';
 		}
 
-		searchParams.delete('page');
-
-		url.search = searchParams.toString();
+		request.page = 0;
+		invalidate(request);
 	}
 
-	function changeOrder(order: string) {
-		let url = $page.url;
-		let searchParams = new URLSearchParams(url.search);
+	function changeOrder(order: 'ascending' | 'descending') {
+		request.order = order;
+		request.page = 0;
 
-		searchParams.set('order', order);
-
-		url.search = searchParams.toString();
+		invalidate(request);
 	}
 
 	function onFilterFavorite() {
@@ -180,32 +184,45 @@
 		response.tag_favorite = json.favorite;
 	}
 
-	function onSearchClick(t: string) {
-		let searchText = t;
-		let url = $page.url;
-		let searchParams = new URLSearchParams(url.search);
-		searchParams.set('search', searchText);
-
-		url.search = searchParams.toString();
-	}
-
-	function onAboutClick() {
-		aboutDialog.show();
+	let searchText = '';
+	function onSearchClick() {
+		request.search = searchText;
+		invalidate(request);
 	}
 
 	function onPageClick(i: number): void {
-		const params = $page.url.searchParams;
-		if ($page.url.searchParams.has('page')) {
-			$page.url.searchParams.delete('page');
-		}
-
-		$page.url.searchParams.set('page', i.toString());
-		promise = loadData();
+		request.page = i;
+		invalidate(request);
 	}
 
 	let navbarToggleOpen = false;
 	function handleUpdate(event: CustomEvent<boolean>) {
 		navbarToggleOpen = event.detail;
+	}
+
+	function invalidate(req: Request) {
+		request = req;
+
+		$page.url.searchParams.set('favorite_only', `${request.favorite_only}`);
+		$page.url.searchParams.set('order', request.order);
+		$page.url.searchParams.set('page', request.page.toString());
+
+		if (request.search == '') {
+			$page.url.searchParams.delete('search');
+		} else {
+			$page.url.searchParams.set('search', request.tag);
+		}
+
+		$page.url.searchParams.set('sort', request.sort);
+
+		if (request.tag == '') {
+			$page.url.searchParams.delete('tag');
+		} else {
+			$page.url.searchParams.set('tag', request.tag);
+		}
+
+		goto($page.url);
+		promise = loadData();
 	}
 </script>
 
@@ -213,43 +230,64 @@
 	<div><Spinner type="grow" /> Loading ...</div>
 {:then}
 	<Navbar color="dark" dark expand="md" sticky={'top'}>
-		<NavbarBrand href="/">{request.tag == '' ? 'Browse' : `Browse ${request.tag}`}</NavbarBrand>
+		<NavbarBrand href="/">{request.tag == '' ? 'Browse' : `Browse: ${request.tag}`}</NavbarBrand>
 		<NavbarToggler on:click={() => (navbarToggleOpen = !navbarToggleOpen)} />
 		<Collapse isOpen={navbarToggleOpen} navbar expand="md" on:update={handleUpdate}>
 			<Nav navbar>
 				<Dropdown nav inNavbar>
 					<DropdownToggle nav caret>Browse</DropdownToggle>
 					<DropdownMenu end>
-						<DropdownItem><Icon name="list-ul" class="me-3" /> All items</DropdownItem>
-						<DropdownItem><Icon name="tags-fill" class="me-3" /> Tag list</DropdownItem>
+						<DropdownItem on:click={()=> {request = defaultRequest(); invalidate(request)}}>
+							<Icon name="list-ul" class="me-3" /> All items
+						</DropdownItem>
+						<DropdownItem on:click={() => goto(new URL('/tags', $page.url.origin).toString())}>
+							<Icon name="tags-fill" class="me-3" /> Tag list
+						</DropdownItem>
 					</DropdownMenu>
 				</Dropdown>
 				<Dropdown nav inNavbar>
 					<DropdownToggle nav caret>Sort By</DropdownToggle>
 					<DropdownMenu end>
-						<DropdownItem><Icon name="type" class="me-3" /> Name</DropdownItem>
-						<DropdownItem><Icon name="clock" class="me-3" /> Create Time</DropdownItem>
+						<DropdownItem active={request.sort == 'name'} on:click={() => changeSort('name')}>
+							<Icon name="type" class="me-3" /> Name
+						</DropdownItem>
+						<DropdownItem
+							active={request.sort == 'createTime'}
+							on:click={() => changeSort('createTime')}
+						>
+							<Icon name="clock" class="me-3" /> Create time
+						</DropdownItem>
 						<DropdownItem divider />
-						<DropdownItem><Icon name="sort-down-alt" class="me-3" />Ascending</DropdownItem>
-						<DropdownItem><Icon name="sort-up-alt" class="me-3" /> Descending</DropdownItem>
+						<DropdownItem
+							active={request.order == 'ascending'}
+							on:click={() => changeOrder('ascending')}
+						>
+							<Icon name="sort-down-alt" class="me-3" />Ascending
+						</DropdownItem>
+						<DropdownItem
+							active={request.order == 'descending'}
+							on:click={() => changeOrder('descending')}
+						>
+							<Icon name="sort-up-alt" class="me-3" /> Descending
+						</DropdownItem>
 					</DropdownMenu>
 				</Dropdown>
 				<NavItem>
-					<NavLink on:click={()=>aboutDialog.show()}>About</NavLink>
+					<NavLink on:click={() => aboutDialog.show()}>About</NavLink>
 				</NavItem>
 			</Nav>
 			<Nav class="ms-auto" navbar>
-				<NavItem>
-					<Button>
-						<Icon name="star-fill" class=" me-3" /> Favorite tag
+				<NavItem hidden={request.tag != ''}>
+					<Button on:click={() => onTagFavorite()}>
+						<Icon name="star-fill" class="me-3" /> Favorite tag
 					</Button>
 				</NavItem>
 			</Nav>
 			<Nav navbar>
 				<NavItem>
 					<InputGroup>
-						<Input type="text" />
-						<Button>Search</Button>
+						<Input type="text" bind:value={request.search} />
+						<Button on:click={() => onSearchClick()}>Search</Button>
 					</InputGroup>
 				</NavItem>
 			</Nav>

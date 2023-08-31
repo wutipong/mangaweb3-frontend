a7f3a5c2d493ea9ac599818214ea5af60eb96b09
Wutipong Wongsakuldej <mr_tawan@hotmail.com>
2023-09-01 03:05:43 +0700

    add configurable default sortfield/order support.

--- src/lib/variables.ts
+++ src/lib/variables.ts
@@ -1,5 +1,7 @@
 import { env } from '$env/dynamic/public';
 
 export const variables = {
-    basePath: env.PUBLIC_BACKEND_URL
+    basePath: env.PUBLIC_BACKEND_URL,
+    defaultSortField: env.PUBLIC_DEFAULT_SORT_FIELD,
+    defaultOrder: env.PUBLIC_DEFAULT_ORDER,
 };

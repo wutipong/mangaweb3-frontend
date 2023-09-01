df383db2ec8a85edd21aa2a8b89c27aec9a974f2
Wutipong Wongsakuldej <mr_tawan@hotmail.com>
2023-09-01 23:42:48 +0700

    replace view data loading to page load.

---
+++ src/routes/view/+page.ts
@@ -0,0 +1,50 @@
+import type { PageLoad } from './$types';
+import { variables } from '$lib/variables';
+import type { Tag } from '$lib/tag';
+
+interface Request {
+    name: string;
+}
+
+interface Response {
+    browse_url: string;
+    favorite: boolean;
+    page_count: number;
+    tags: Tag[];
+}
+
+let request: Request = {
+    name: ''
+};
+
+let response: Response = {
+    browse_url: '',
+    favorite: false,
+    page_count: 0,
+    tags: []
+};
+
+export const prerender = false;
+
+export const load: PageLoad = async ({ fetch, url }) => {
+    const params = url.searchParams;
+    if (params.has('name')) {
+        request.name = params.get('name') as string;
+    }
+
+    const resp = await fetch(
+        new URL('/view', variables.basePath),
+        {
+            method: 'POST',
+            body: JSON.stringify(request)
+        }
+    );
+    response = await resp.json();
+
+    return {
+        name: request.name,
+        favorite: response.favorite,
+        pageCount: response.page_count,
+        tags: response.tags,
+    };
+};

74e4f62ce814eec15528ded4e08b54ca4eff44b6
Wutipong Wongsakuldej <mr_tawan@hotmail.com>
2024-12-02 05:05:23 +0700

    update favicon

---
+++ src/routes/manifest.webmanifest/+server.ts
@@ -0,0 +1,30 @@
+import { assets } from '$app/paths';
+import type { RequestHandler } from './$types';
+
+const manifest = `{
+    "name":"Mangaweb3",
+    "short_name":"mangaweb3",
+    "icons":[
+        {
+            "src":"${assets}/android-chrome-192x192.png",
+            "sizes":"192x192","type":"image/png"
+        },
+        {
+            "src":"${assets}/android-chrome-512x512.png",
+            "sizes":"512x512","type":"image/png"
+        }
+    ],
+    "theme_color":"#ffffff",
+    "background_color":"#ffffff",
+    "display":"standalone"
+}`
+
+export const GET: RequestHandler = async () => {
+    return new Response(JSON.stringify(manifest), {
+        headers: {
+            'content-type': 'application/manifest+json',
+        },
+    });
+};
+
+export const prerender = true; // this line will generate a static manifest.webmanifest

3be93416146a8d9e77d1c2e441638ce386b3a51e
Wutipong Wongsakuldej <mr_tawan@hotmail.com>
2025-07-15 00:39:28 +0700

    add login + login callback

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import { Container, Button } from "@sveltestrap/sveltestrap";
+</script>
+
+<svelte:head>
+	<title>MangaWeb 4 Login</title>
+</svelte:head>
+
+<Container>
+    <h1 class="text-center mt-5">MangaWeb 4</h1>
+    <p class="fluid text-center mt-5">Login using OpenID Connect.</p>
+
+    <div class="fluid text-center"><Button color="primary" size="lg" href="https://auth.sleepyhead.name/application/o/authorize/?response_type=code&scope=openid%20email%20offline_access&client_id=SZ87KOccOuVlpqUBmPYHk4G9OcbRfw3CXNFzS4v5&redirect_uri=http%3A%2F%2Flocalhost%3A5173%2Flogin%2Fcallback&audience=http%3A%2F%2Flocalhost%3A5173">Login</Button></div>
+</Container>
---
+++ src/routes/login/callback/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import { Container, Button } from "@sveltestrap/sveltestrap";
+</script>
+
+<svelte:head>
+	<title>MangaWeb 4 Login</title>
+</svelte:head>
+
+<Container>
+    <h1 class="text-center mt-5">MangaWeb 4</h1>
+    <p class="fluid text-center mt-5">Login using OpenID Connect.</p>
+
+    <div class="fluid text-center">Authorized</div>
+</Container>
